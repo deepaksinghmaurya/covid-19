a465371fe848326ff8f157c4ac7d2a1506c0eedb
Deepak Singh Maurya <mauryagdeepak@gmail.com>
2020-08-13 14:17:14 +0530

    Updates

---
+++ precache-manifest.d9a23bb57c584e620769c90d56f4b7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc516707f36172b847510794c93a3e8",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f6a415f7e15afce69003",
+    "url": "/covid-19/static/css/main.38dd4fc0.chunk.css"
+  },
+  {
+    "revision": "8c643d7278208554b0b3",
+    "url": "/covid-19/static/js/2.0a97a583.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19/static/js/2.0a97a583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a415f7e15afce69003",
+    "url": "/covid-19/static/js/main.bf2cd3dd.chunk.js"
+  },
+  {
+    "revision": "d46fff10edf104623014",
+    "url": "/covid-19/static/js/runtime-main.6f57abcd.js"
+  }
+]);
